56dfbb045b412926138a39e6713fd9323a5baac0
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-01-05 00:15:03 +0200

    Adding the missing interface description

--- src/interfaces/database.ts
+++ src/interfaces/database.ts
@@ -1,3 +1,28 @@
+/**
+ * The parameters used to create a database
+ * @param dryRun Optional. When 'false’: Creates a deployment plan and deploys it (creating any resources required by the plan). When 'true’: creates a read-only deployment plan without any resource creation. Default: ‘true’
+ * @param name Required. Database name
+ * @param protocol Optional. Database protocol: either ‘redis’ or 'memcached’. Default: ‘redis’
+ * @param memoryLimitInGb Required. Maximum memory usage for this specific database
+ * @param supportOSSClusterApi Optional. Support Redis open-source (OSS) Cluster API
+ * @param useExternalEndpointForOSSClusterApi Optional. Should use external endpoint for open-source (OSS) Cluster API. Can only be enabled if OSS Cluster API support is enabled’. Default: ‘false’
+ * @param dataPersistence Optional. Rate of database data persistence (in persistent storage)
+ * @param dataEvictionPolicy Optional. Data items eviction method
+ * @param replication Optional. Databases replication
+ * @param throughputMeasurement Optional. The throughput measurement of the database
+ * @param throughputMeasurement.by Required. Throughput measurement method. Either ‘number-of-shards’ or ‘operations-per-second’
+ * @param throughputMeasurement.value Required. Throughput value (as applies to selected measurement method)
+ * @param replicaOf Optional. This database will be a replica of the specified Redis databases provided as one or more URI (sample format: 'redis://user:password@host:port)'. If the URI provided is Redis Labs Cloud instance, only host and port should be provided (using the format: ['redis://endpoint1:6379’, ‘redis://endpoint2:6380’] ).
+ * @param periodicBackupPath Optional. If specified, database will be able to perform backups to this path. If empty string is received, backup path will be removed
+ * @param sourceIp Optional. List of source IP addresses or subnet masks. If specified, Redis clients will be able to connect to this database only from within the specified source IP addresses ranges (example: ['192.168.10.0/32’, ‘192.168.12.0/24’] )
+ * @param clientSslCertificate Optional. If specified, this SSL certificate will be required to authenticate user connections. If empty string is received, SSL certificate will be removed
+ * @param password Optional. If specified, this password will be used to access the database
+ * @param alerts Optional. Redis Labs database alerts
+ * @param averageItemSizeInBytes Optional. Relevant only to ram-and-flash subscriptions. Estimated average size (measured in bytes) of the items stored in the database, Default: 1000
+ * @param modules Optional. Redis Labs modules to be provisioned in the database
+ * @param modules.name Required. Redis Labs module Id
+ * @param modules.parametersOptional Redis Labs database module parameters (name and value), as relevant to the specific module (see modules parameters specification)
+ */
 export interface CreateDatabaseParameters {
     dryRun?: boolean,
     name: string,
