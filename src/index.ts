ba47f81708f53d6c7912b1c5bb8337bfa7874a3b
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-01-05 23:39:59 +0200

    Exporting all of the functions, interfaces and types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export { CloudAPISDK } from './api';
+export { CloudAPISDK, CloudAPISDKParameters } from './api';
+export { CreateCloudAccountParameters, UpdateCloudAccountParameters } from './interfaces/cloud-account';
+export { CreateDatabaseParameters, UpdateDatabaseParameters, DatabaseImportParameters } from './interfaces/database';
+export { CreateSubscriptionParameters, UpdateSubscriptionParameters, CreateSubscriptionVpcPeeringParameters, UpdateSubscriptionCidrWhitelistParameters } from './interfaces/subscription';
+export { CloudAccountProviderTypes } from './types/cloud-account';
+export { DatabaseProtocolTypes, DatabaseDataEvictionPolicyTypes, DatabaseDataPersistenceTypes, DatabaseImportSourceTypes, DatabaseThroughputMeasurementByTypes } from './types/database';
+export { SubscriptionCloudProviderTypes, SubscriptionMemoryStorageTypes } from './types/subscription';
