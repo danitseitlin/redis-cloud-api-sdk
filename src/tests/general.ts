7aeff7a23cc028f97c9bc0c5124b63ff1e1e9efa
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-01-12 21:33:26 +0200

    Adding tests for the general requests

---
+++ src/tests/general.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+import { CloudAPISDK, CloudAPISDKParameters } from '../api';
+import { CreateCloudAccountParameters } from '../interfaces/cloud-account'
+const cloudAPISDKParameters: CloudAPISDKParameters = {
+    accessKey: 'your-access-key',
+    secretKey: 'your-secret-key'
+}
+const cloudAPIClient: CloudAPISDK = new CloudAPISDK(cloudAPISDKParameters);
+describe('Testing cloud account', async function() {
+    it('getAccountInformation', async function() {
+        const accountInformation: any = await cloudAPIClient.getAccountInformation();
+        expect(accountInformation['account']).not(undefined, 'Checking if account key exists');
+    }); 
+    it('getDatabaseModules', async function() {
+        const databaseModules: any = await cloudAPIClient.getDatabaseModules();
+        expect(databaseModules['modules']).not(undefined, 'Checking if modules key exists');
+    })
+    it('getSystemLogs', async function() {
+        const systemLogs: any = await cloudAPIClient.getSystemLogs(100, 0);
+        expect(systemLogs['enteries']).not(undefined, 'Checking if enteries key exists');
+    }); 
+    it('getPaymentMethods', async function() {
+        const paymentMethods: any = await cloudAPIClient.getPaymentMethods();
+        expect(paymentMethods['paymentMethods']).not(undefined, 'Checking if paymentMethods key exists');
+    })
+    it('getPlans', async function() {
+        const paymentMethods: any = await cloudAPIClient.getPlans('AWS');
+        expect(paymentMethods['plans']).not(undefined, 'Checking if plans key exists');
+    })
+    it('getRegions', async function() {
+        const paymentMethods: any = await cloudAPIClient.getRegions('AWS');
+        expect(paymentMethods['regions']).not(undefined, 'Checking if regions key exists');
+    })
+  });
