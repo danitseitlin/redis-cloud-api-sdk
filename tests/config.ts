0f2beaf9e92a0903716a2f0e3eba9f81aae898d1
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-02-01 08:43:54 +0200

    Set up a configuration file for easier usage on test execution

---
+++ tests/config.ts
@@ -0,0 +1,6 @@
+export const TEST_CONFIG = {
+    API_ACCESS_KEY: 'your-cloud-api-access-key',
+    API_SECRET_KEY: 'your-cloud-api-secret-key',
+    AWS_ACCESS_ID: 'your-aws-access-key-id',
+    AWS_SECRET_KEY: 'your-aws-secret-key'
+}
