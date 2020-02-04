9738cf4c9e2020213ea27f2a255c912dd9dfc99a
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-02-04 23:46:47 +0200

    Adding more values to the test config file

--- tests/config.ts
+++ tests/config.ts
@@ -3,5 +3,8 @@ export const TEST_CONFIG = {
     API_SECRET_KEY: 'your-cloud-api-secret-key',
     AWS_ACCESS_ID: 'your-aws-access-key-id',
     AWS_SECRET_KEY: 'your-aws-secret-key',
+    VPC_PEERING_ID: 'your-vpc-peering-id',
+    VPC_PEERING_CIDR: 'your-vpc-peering-cidr',
+    VPC_PEERING_AWS_ACCOUNT_ID: 'your-vpc-peering-aws-account-id',
     ENVIRONMENT: 'your-testing-environment-if-needed'
 }
