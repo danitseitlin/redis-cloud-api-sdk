9df5f0575e63dbb4aa1d8c4d5a7e93af3a6b7b18
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-02-26 23:06:18 +0200

    Adding a console.log for the response message

--- tests/unit/cloud-account.ts
+++ tests/unit/cloud-account.ts
@@ -46,6 +46,7 @@ describe('Testing cloud account', async function() {
     });
     it('deleteCloudAccount', async () => {
         const response = await cloudAPIClient.deleteCloudAccount(cloudAccountId);
+        console.log(response.message);
         expect(response.message).to.eql(undefined, 'Error message existence');
         expect(response.message).not.to.eql('Request failed with status code 404', 'Error message type');
     });
