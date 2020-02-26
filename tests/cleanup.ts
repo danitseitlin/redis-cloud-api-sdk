072b9e2f3e4adfdefec80f2bd4680d2b50c9687a
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-02-26 21:23:35 +0200

    Fixing the cleanup tests and some improvements

--- tests/cleanup.ts
+++ tests/cleanup.ts
@@ -14,40 +14,36 @@ const cloudAPIClient: CloudAPISDK = new CloudAPISDK(cloudAPISDKParameters);
 describe('Cleanup', async function() {
     this.timeout(1000 * 60 * 60);
     
-    it('Remove subscriptions', async () => {
-        const subscriptions = await cloudAPIClient.getSubscriptions();
+    it('Resources cleanup', async () => {
+        let subscriptions = await cloudAPIClient.getSubscriptions();
         for(let i = 0; i < subscriptions.length; i++) {
             const subscriptionId: number = subscriptions[i]['id'];
-            const databases = await cloudAPIClient.getDatabases(subscriptionId);
+            let databases = await cloudAPIClient.getDatabases(subscriptionId);
             for(let j = 0; j < databases.length; j++) {
                 const databaseId: number = databases[j]['databaseId'];
-                const deleteDatabaseResponse: any = await cloudAPIClient.deleteDatabase(subscriptionId, databaseId);
-                // if(TEST_ARGUMENTS.DEBUG) console.log(deleteDatabaseResponse);
+                await cloudAPIClient.deleteDatabase(subscriptionId, databaseId);
                 await cloudAPIClient.waitForDatabaseStatus(subscriptionId, databaseId, DatabaseStatus.deleted);
-                // const database = await cloudAPIClient.getDatabase(subscriptionId, databaseId);
-                // if(TEST_ARGUMENTS.DEBUG) console.log(database);
-                // const error: any = database.find((error: any) => error['status'] !== undefined);  
-                // if(TEST_ARGUMENTS.DEBUG) console.log(error);
-                // expect(error['status']).to.eql('Not Found', 'Checking that the database was deleted');
             }
-        
-            const deleteSubscriptionResponse: any = await cloudAPIClient.deleteSubscription(subscriptionId);
-            // if(TEST_ARGUMENTS.DEBUG) console.log(deleteSubscriptionResponse);
+            databases = await cloudAPIClient.getDatabases(subscriptionId);
+            expect(subscriptions.length).to.eql(0, `Databases count for subsription ${subscriptionId}`);
+            await cloudAPIClient.deleteSubscription(subscriptionId);
             await cloudAPIClient.waitForSubscriptionStatus(subscriptionId, SubscriptionStatus.deleted);
-            // const subscription: any = await cloudAPIClient.getSubscription(subscriptionId);
-            // const error: any = subscription.find((error: any) => error['status'] !== undefined);  
-            // if(TEST_ARGUMENTS.DEBUG) console.log(error);
-            // expect(error['status']).to.eql('Not Found', 'Subscription was not removed');
+
         }
+        subscriptions = await cloudAPIClient.getSubscriptions();
+        expect(subscriptions.length).to.eql(0, 'Subscriptions count');
     });
 
-    it('Remove cloud accounts', async () => {
-        const cloudAccounts = await cloudAPIClient.getCloudAccounts();
-        let cloudAccount = cloudAccounts.find(cloudAccount => cloudAccount['id'] !== -1);
-        const cloudAccountId = cloudAccount['id'];
-        await cloudAPIClient.deleteCloudAccount(cloudAccountId);
-        await cloudAPIClient.waitForCloudAccountStatus(cloudAccountId, CloudAccountStatus.deleted);
-        cloudAccount = await cloudAPIClient.getCloudAccount(cloudAccountId);
-        expect(cloudAccount['status']).to.not.eql(CloudAccountStatus.active, 'Cloud Account Status');
+    it('Cloud account cleanup', async () => {
+        let cloudAccounts = await cloudAPIClient.getCloudAccounts();
+        for(let i = 0; i < cloudAccounts.length; i++) {
+            const cloudAccountId = cloudAccounts[i]['id'];
+            await cloudAPIClient.deleteCloudAccount(cloudAccountId);
+            await cloudAPIClient.waitForCloudAccountStatus(cloudAccountId, CloudAccountStatus.deleted);
+            const cloudAccount = await cloudAPIClient.getCloudAccount(cloudAccountId);
+            expect(cloudAccount['status']).to.not.eql(CloudAccountStatus.active, 'Cloud Account Status');
+        }
+        cloudAccounts = await cloudAPIClient.getCloudAccounts();
+        expect(cloudAccounts.length).to.eql(0, 'Cloud accounts count');
     });
 });
