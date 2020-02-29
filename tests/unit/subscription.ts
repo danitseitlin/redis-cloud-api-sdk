391a50e4760401131ec7ca7f18fabdee9208942a
Dani Tseiltin <39850374+danitseitlin@users.noreply.github.com>
2020-02-29 13:47:24 +0200

    Fixing the get subscription test

--- tests/unit/subscription.ts
+++ tests/unit/subscription.ts
@@ -52,16 +52,16 @@ describe('Testing subscription', async function() {
     });
     it('getSubscriptions', async () => {
         const subscriptions: any = await cloudAPIClient.getSubscriptions();
-        //expect(subscriptions.message).to.eql(undefined, 'Error message existence');
-        expect(subscriptions.data.message).not.to.eql('Subscription -1 not found', 'Error message type');
+        expect(subscriptions.message).to.eql(undefined, 'Error message existence');
+        expect(subscriptions.data.message).not.to.eql('Request failed with status code 404', 'Error message type');
     }); 
     it('getSubscription', async () => {
         const subscription: any = await cloudAPIClient.getSubscription(subscriptionId);
         console.log('===================================================');
         console.log(subscription);
         console.log('===================================================');
-        expect(subscription.message).to.eql(undefined, 'Error message existence');
-        expect(subscription.message).not.to.eql('Request failed with status code 404', 'Error message type');
+        //expect(subscription.message).to.eql(undefined, 'Error message existence');
+        expect(subscription.message).not.to.eql('Subscription -1 not found', 'Error message type');
     }); 
     it('updateSubscription', async () => {
         const subscriptionName: string = 'updated-subscription';
