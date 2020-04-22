63d4fa4bc4598c321859cbcf57c793682e753aaf
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-04-23 01:20:35 +0300

    Removing debug lines

--- tests/integration/general.ts
+++ tests/integration/general.ts
@@ -18,35 +18,24 @@ describe('Testing general functions', async function() {
     }); 
     it('getDatabaseModules', async () => {
         const databaseModules = await cloudAPIClient.getDatabaseModules();
-        console.log('============= Database modules =============');
-        console.log(databaseModules[1].parameters);
-        console.log('============================================');
         expect(databaseModules.length).gte(4, 'Database modules count');
     });
     it('getSystemLogs', async () => {
         const systemLogs = await cloudAPIClient.getSystemLogs(2, 0);
-        console.log('============= System logs =============');
-        console.log(systemLogs);
-        console.log('=======================================');
         expect(systemLogs.length).gte(0, 'System logs count');
     }); 
     it('getPaymentMethods', async () => {
         const paymentMethods = await cloudAPIClient.getPaymentMethods();
-        console.log('============= Payment methods =============');
-        console.log(paymentMethods);
-        console.log('===========================================');
         expect(paymentMethods.length).gte(0, 'Payment methods count');
     });
     it('getPlans', async () => {
         const AWSPlans = await cloudAPIClient.getPlans('AWS');
-        console.log(AWSPlans);
         expect(AWSPlans.length).gte(0, 'AWS plans count');
         const GCPPlans = await cloudAPIClient.getPlans('GCP');
         expect(GCPPlans.length).gte(0, 'GCP plans count');
     });
     it('getRegions', async () => {
         const AWSRegions = await cloudAPIClient.getRegions('AWS');
-        console.log(AWSRegions);
         expect(AWSRegions.length).gte(0, 'AWS regions count');
         const GCPRegions = await cloudAPIClient.getRegions('GCP');
         expect(GCPRegions.length).gte(0, 'GCP regions count');
