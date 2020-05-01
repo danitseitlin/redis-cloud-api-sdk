34ea3aafc96f50b97365e1e214f679c41a6c2100
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-05-01 23:52:58 +0300

    Fixing the general unit testing

--- tests/unit/general.ts
+++ tests/unit/general.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { CloudAPISDK, CloudAPISDKParameters } from '../../src/api';
+import { CloudAPISDK } from '../../src/api';
 import { loadArguments } from '../helpers';
 import { MockServer } from 'dmock-server';
 
@@ -11,13 +11,12 @@ const server = new MockServer({
     routes: mock.routes
 });
 
-const cloudAPISDKParameters: CloudAPISDKParameters = {
+const cloudAPIClient: CloudAPISDK = new CloudAPISDK({
     protocol: 'http',
     domain: `${testArguments.ENVIRONMENT}:${testArguments.PORT}`,
     accessKey: testArguments.API_ACCESS_KEY,
     secretKey: testArguments.API_SECRET_KEY,
-}
-const cloudAPIClient: CloudAPISDK = new CloudAPISDK(cloudAPISDKParameters);
+});
 describe('Testing general functions', async function() {
     this.timeout(10 * 60 * 60);
     before(async () => {
@@ -29,38 +28,26 @@ describe('Testing general functions', async function() {
     });
     it('getAccountInformation', async () => {
         const accountInformation = await cloudAPIClient.getAccountInformation();
-        expect(accountInformation.message).to.eql(undefined, 'Error message existence');
-        expect(accountInformation.message).not.to.eql('Request failed with status code 404', 'Error message type');
+        expect(accountInformation.key.accountId).to.eql(123, 'Account id');
     }); 
     it('getDatabaseModules', async () => {
         const databaseModules = await cloudAPIClient.getDatabaseModules();
-        expect(databaseModules.message).to.eql(undefined, 'Error message existence');
-        expect(databaseModules.message).not.to.eql('Request failed with status code 404', 'Error message type');
+        expect(databaseModules.length).to.eql(1, 'Database modules count')
     });
     it('getSystemLogs', async () => {
         const systemLogs = await cloudAPIClient.getSystemLogs(2, 0);
-        expect(systemLogs.message).to.eql(undefined, 'Error message existence');
-        expect(systemLogs.message).not.to.eql('Request failed with status code 404', 'Error message type');
+        expect(systemLogs.length).to.eql(1, 'System logs count')
     }); 
     it('getPaymentMethods', async () => {
         const paymentMethods = await cloudAPIClient.getPaymentMethods();
-        expect(paymentMethods.message).to.eql(undefined, 'Error message existence');
-        expect(paymentMethods.message).not.to.eql('Request failed with status code 404', 'Error message type');
+        expect(paymentMethods.length).to.eql(1, 'Payment methods count');
     });
     it('getPlans', async () => { 
-        const AWSPlans = await cloudAPIClient.getPlans('AWS');
-        expect(AWSPlans.message).to.eql(undefined, 'Error message existence');
-        expect(AWSPlans.message).not.to.eql('Request failed with status code 404', 'Error message type');
-        const GCPPlans = await cloudAPIClient.getPlans('GCP');
-        expect(GCPPlans.message).to.eql(undefined, 'Error message existence');
-        expect(GCPPlans.message).not.to.eql('Request failed with status code 404', 'Error message type');
+        const plans = await cloudAPIClient.getPlans('AWS');
+        expect(plans.length).to.eql(1, 'Plans count');
     });
     it('getRegions', async () => {
-        const AWSRegions = await cloudAPIClient.getRegions('AWS');
-        expect(AWSRegions.message).to.eql(undefined, 'Error message existence');
-        expect(AWSRegions.message).not.to.eql('Request failed with status code 404', 'Error message type');
-        const GCPRegions = await cloudAPIClient.getRegions('GCP');
-        expect(GCPRegions.message).to.eql(undefined, 'Error message existence');
-        expect(GCPRegions.message).not.to.eql('Request failed with status code 404', 'Error message type');
+        const regions = await cloudAPIClient.getRegions('AWS');
+        expect(regions.length).to.eql(1, 'Regions count');
     });
 });
