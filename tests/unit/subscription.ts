ec2718c9710fc60e64524c874ed897f4827707cc
Dani Tseitlin <dani.tseitlin@gmail.com>
2020-05-15 21:42:27 +0300

    Rebuilding the structure of the folders

--- tests/unit/subscription.ts
+++ tests/unit/subscription.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { CloudAPISDK } from '../../src/api';
+import { CloudAPISDK } from '../../api';
 import { loadArguments } from '../helpers';
 import { MockServer } from 'dmock-server';
 
