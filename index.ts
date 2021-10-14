8dbb4a43a23244e0b035024dee926d2f79456a6d
michaelk-redis <77917588+michaelk-redis@users.noreply.github.com>
2021-10-14 10:09:54 +0300

    Add Alert to export in index.ts (#145)

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@
 export { CloudAPISDK, CloudAPISDKParameters } from './api';
 // PARAMETERS
 export { CloudAccountCreationParameters, CloudAccountUpdateParameters } from './types/parameters/cloud-account';
-export { DatabaseCreationParameters, DatabaseUpdateParameters, DatabaseImportParameters, Module } from './types/parameters/database';
+export { DatabaseCreationParameters, DatabaseUpdateParameters, DatabaseImportParameters, Module, Alert } from './types/parameters/database';
 export { CreateSubscriptionParameters, SubscriptionUpdateParameters, VpcPeeringCreationParameters, CidrUpdateParameters, CloudProvider, Database, CloudProviderRegion } from './types/parameters/subscription';
 // RESPONSES
 export { CloudAccountProvider } from './types/responses/cloud-account';
