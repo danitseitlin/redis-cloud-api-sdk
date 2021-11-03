4612e0e5e7f9ff94af03ef4c5f8ef00872e0bfe6
Michael Kislansky <michael.kislansky@redislabs.com>
2021-11-03 09:52:34 +0200

    add id & version

--- types/responses/general.ts
+++ types/responses/general.ts
@@ -50,7 +50,9 @@ export type DataPersistence = {
 
 /**
  * Database module object
+ * @param id The ID of the module
  * @param name The name of the module
+ * @param version The version of the module
  * @param description The description of the module
  * @param parameters The parameters of the module
  */
